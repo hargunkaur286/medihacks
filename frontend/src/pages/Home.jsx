08e2d6f6c7b9f46b704dc58ea5b1d6336822e151
Hargun Kaur <hargunkaur286@gmail.com>
2024-07-08 19:54:10 +0530

    How it works

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import heroImg01 from "../assets/images/hero-img01.png"
 import heroImg02 from "../assets/images/hero-img02.png"
 import heroImg03 from "../assets/images/hero-img03.png"
-
-
+import icons1 from "../assets/images/icon01.png"
+import icons2 from "../assets/images/icon02.png"
+import icons3 from "../assets/images/icon03.png"
+import {Link} from 'react-router-dom'
+import { BiRightArrowAlt } from "react-icons/bi";
 const Home = () => {
   return (
     <>
@@ -83,6 +86,69 @@ const Home = () => {
             <h2 className='heading text-center'>
               Providing the best medical services
             </h2>
+            <p className="text__para text-center">
+              World-class care for everyone. Our health system offeres unmatched, expert health care.
+            </p>
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]">
+            <div className='py-[30px] px-5'>
+              <div className="flex items-center justify-center">
+                <img src={icons1} alt="" />
+              </div>
+
+              <div className='mt-[30px]'>
+                <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>
+                  Find a Doctor
+                </h2>
+                <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>
+                  World-class care for everyone. Our health system offers unmatched, expert health care. From the lab to the clinic. 
+                </p>
+
+                <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
+                  <BiRightArrowAlt className=" group-hover:text-white w-6 h-5" />
+                </Link>
+              </div>
+            </div>
+
+            <div className='py-[30px] px-5'>
+              <div className="flex items-center justify-center">
+                <img src={icons2} alt="" />
+              </div>
+
+              <div className='mt-[30px]'>
+                <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>
+                  Find a Location
+                </h2>
+                <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>
+                  World-class care for everyone. Our health system offers unmatched, expert health care. From the lab to the clinic. 
+                </p>
+
+                <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
+                  <BiRightArrowAlt className=" group-hover:text-white w-6 h-5" />
+                </Link>
+              </div>
+            </div>
+
+            <div className='py-[30px] px-5'>
+              <div className="flex items-center justify-center">
+                <img src={icons3} alt="" />
+              </div>
+
+              <div className='mt-[30px]'>
+                <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>
+                  Book and Appointment
+                </h2>
+                <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>
+                  World-class care for everyone. Our health system offers unmatched, expert health care. From the lab to the clinic. 
+                </p>
+
+                <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
+                  <BiRightArrowAlt className="group-hover:text-white w-6 h-5" />
+                </Link>
+              </div>
+            </div>
+
           </div>
         </div>
       </section>
