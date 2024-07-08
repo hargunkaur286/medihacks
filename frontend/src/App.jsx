83182b7666dd596d82d19c95f29561884d3379a5
Hargun Kaur <hargunkaur286@gmail.com>
2024-07-08 13:15:20 +0530

    setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./app.css";
+import Layout from "./layout/Layout";
+
+function App() {
+  return (
+    <>
+     <h1>Hello wassup?</h1>
+     <Layout />
+    </>
+  )
+}
+
+export default App
