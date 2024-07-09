4a5d37573aca5cb230a47c96ba6000db587cf7a6
Hargun Kaur <hargunkaur286@gmail.com>
2024-07-09 20:50:52 +0530

    added testimonials

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -15,6 +15,7 @@ import About from '../components/About/About'
 import ServiceList from '../components/Services/ServiceList'
 import DoctorList from '../components/Doctors/DoctorList'
 import FaqList from '../components/Faq/FaqList'
+import Testimonial from '../components/Testimonial/Testimonial'
 
 const Home = () => {
   return (
@@ -277,6 +278,20 @@ const Home = () => {
         </div>
       </section>
 
+      {/* testimonials */}
+      <section>
+        <div className="container">
+          <div className='xl:w-[470px] mx-auto'>
+            <h2 className='heading text-center'>
+              What our patients say
+            </h2>
+            <p className='text__para text-center'>
+              World-class care for every mother. Our health system offers unmatched, expert health care.
+            </p>
+          </div>
+          <Testimonial />
+        </div>
+      </section>
     </>
   )
 }
