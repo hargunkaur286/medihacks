67a620c57a6a3a44904815ac0b49b8a870dd97b3
Hargun Kaur <hargunkaur286@gmail.com>
2024-07-11 21:50:32 +0530

    chatapp

---
+++ frontend/src/pages/Chatbot.jsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react'
+
+const Chatbot = () => {
+    const [error, setError] = useState("")
+  return (
+    <section className='search-section'>
+        <p>What Do you want to know?
+            <button className="surprise">Surprise Me!</button>
+        </p>
+        <div className="input-container">
+            <input
+                value={}
+                placeholder="Suggest some nutricious food during pregnancy?"
+                onChange={""}
+            />
+            {!error && <button>Ask Me</button>}
+            {error && <button>Clear</button>}
+        </div>
+        {error && <p>{error}</p> }
+        <div className="search-results">
+            <div key={""}>
+                <p className="answer">
+                    
+                </p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Chatbot
